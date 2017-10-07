34fda66bc28b442b5517338788e667694d4e347c
jithuraj902 <jithuraj902@gmail.com>
2017-10-07 13:46:48 +0000

    sampleapp

---
+++ src/main/java/j1/r1/p1/App.java
@@ -0,0 +1,13 @@
+package j1.r1.p1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
